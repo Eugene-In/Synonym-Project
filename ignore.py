e498407ee35928eafaea1d3273110a959f26b9a7
unknown <Matthew>
2020-03-12 10:47:10 -0500

    Just placed the code that was in main in this class, just in case.

--- ignore.py
+++ ignore.py
@@ -24,3 +24,23 @@ class IgnoreList:
       return provide
     else:
       return word
+    '''
+    To be put in main.py if we want to use ignoreList: Matthew
+    while True:
+            try:
+                response = int(input("\nDo you want to ignore any word?:\n (Yes/1, No/2)\n"))
+                if response == 1:
+                    fp = open(r"ignoreList.txt", "w", encoding="utf-8")
+                    fp.write(input("\nPlease input words you don't want\nPut a space between each word.\n"))
+                    fp.close()
+                    break
+                elif response == 2:
+                    fp = open(r"ignoreList.txt", "w", encoding="utf-8")
+                    fp.write("")
+                    fp.close()
+                    break
+                else:
+                    print("\nPlease input 1 or 2 for the ignore list.")
+            except ValueError:
+                print("\nPlease input 1 or 2 for the ignore list.")
+    '''
