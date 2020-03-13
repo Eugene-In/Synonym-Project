ce51fc679b511056a54e42baa737fb8f330be0e7
Alberto <acattaneo@luc.edu>
2020-03-13 15:32:21 -0500

    Added initial synonym scraping for frequent words. No replacement yet but it should be an easy addition. Also, considering spliting this further into multiple functions, although I fear doing so could cause messyness through handing arrays and textlists back and forth between methods and functions instead of holding them in one central place.

--- newParagraphBreaker.py
+++ newParagraphBreaker.py
@@ -11,10 +11,23 @@ class paragraphBreaker:
     with open("testingFileDream.txt",encoding="utf-8") as f:
         data = f.readlines()
     for x in data:
+        #This is the string with no editing done
         print(x)
         loweredArray = x.lower()
         splitArray = re.findall(r"[\w']+", loweredArray)
         
-        freqTable = DataManager.manager(["and","the","a"], splitArray)
+        freqTable = DataManager.manager(["and","the","a","in","i","you"], splitArray)
+        #This is what the word frequency table looks like
         print(freqTable)
         
+        #Do we want to split this into a different class or function. That way one function breaks the paragraph which it then hands to the synonym checker
+        
+        
+        wordsToReplace = []
+        for y in freqTable:
+            if freqTable.get(y) != 1:
+                wordsToReplace.append(y)
+        #Only words that appear more than once need to be considered
+        print(wordsToReplace)
+        for y in wordsToReplace:
+            print("Synonyms for '" + y + "' are: ",syn.synonyms(y))
