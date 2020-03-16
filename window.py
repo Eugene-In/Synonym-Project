3278ba61147522ee511aeec563458b5c94017905
unknown <Matthew>
2020-03-16 16:05:59 -0500

    Fixed it so it makes the data in the if statement, saves time

--- window.py
+++ window.py
@@ -29,23 +29,23 @@ class LookingGlass:
         while cnt < len(holder):
             x = holder[cnt]
             y = x.lower()
-            if len(q) == winlength:
-                q.pop(0)
+            q.pop(0)
             q.append(y)
             cnt += 1
-
             displayCnt += 1
-            #Empty array in manager is for ignoreList
-            data = DataManager.manager([], q)
 
+            if cnt == len(cq):
+                displayCnt = winlength
 
             if displayCnt == winlength:
+                #Empty array in manager is for ignoreList
+                data = DataManager.manager([], q)
                 print("\nsecond instance of printing")
                 print(str(cnt)+"\n")
                 for argh in q:
-                    print(argh)
-                    print(syn.synonyms(argh))
-                    print('\n')
+                    #print(argh)
+                    #print(syn.synonyms(argh))
+                    syn.synonyms(argh)
                 if printArray != None:
                     print(q)
                     print('\n')
@@ -57,3 +57,5 @@ class LookingGlass:
                     print(data)
                 print("\n")
                 displayCnt = 0
+
+        print("done")
