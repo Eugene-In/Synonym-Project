569dbca0eb6ad9308e16fcac7a2b0211857a36e9
Alberto <acattaneo@luc.edu>
2020-03-17 21:25:18 -0500

    Pushing my version of the code again

--- newParagraphBreaker.py
+++ newParagraphBreaker.py
@@ -2,55 +2,28 @@ import sys
 import re
 import dictionaryHandler as syn
 import string
+import re
 from ignore import IgnoreList
 from dataManager import DataManager
 from window import LookingGlass
 
 class paragraphBreaker:
-    document = []
-    window = int(input("Window Size:\n"))
-    with open("testingFileDream.txt",encoding="utf-8") as fp:
-        line = fp.readline()
-        while line:
-            qwe = line.replace('_'," ")
-            b = re.findall(r"\w+", qwe)
-            document.extend(b)
-            line = fp.readline()
-        fp.close()
-    print(document)
-
-    if len(document) < window:
-        window = len(document)
-
-    cnt = 0
-    lcnt = 0
-    data = []
-    para = []
-    for x in document:
-        if cnt < window:
-            para.append(x)
-            cnt += 1
-            lcnt += 1
-
-        if lcnt == len(document):
-            data.append(para)
-            cnt = 0
-        else:
-            data.append(para)
-            cnt = 0
-            lcnt += 1
-
+    with open("testingFileDream.txt",encoding="utf-8") as f:
+        data = f.readlines()
     for x in data:
-        cnt += 1
-        print("Run #"+str(cnt)+"\n")
-        freqTable = DataManager.manager(["and","the","a","in","i","you"], x)
+        #This is the string with no editing done
+        print(x)
+        loweredArray = x.lower()
+        #this is where the lines are being broken down into arrays. THIS IS WHERE WE WOULD INSERT DIFFERENT SPLIT SYSTEMS
+        splitArray = re.findall(r"[\w']+", loweredArray)
+        
+        freqTable = DataManager.manager(["and","the","a","in","i","you"], splitArray)
         #This is what the word frequency table looks like
-        print("\n")
         print(freqTable)
-
+        
         #Do we want to split this into a different class or function. That way one function breaks the paragraph which it then hands to the synonym checker
-
-
+        
+        
         wordsToReplace = []
         for y in freqTable:
             if freqTable.get(y) != 1:
@@ -58,5 +31,4 @@ class paragraphBreaker:
         #Only words that appear more than once need to be considered
         print(wordsToReplace)
         for y in wordsToReplace:
-            print("\n")
             print("Synonyms for '" + y + "' are: ",syn.synonyms(y))
