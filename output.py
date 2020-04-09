7ce8b20c582da6de91e811a3d60bf8cf755fbc6d
unknown <Matthew>
2020-04-09 17:22:33 -0500

    This is the new file writer

---
+++ output.py
@@ -0,0 +1,21 @@
+class WriteNewFile:
+    def fileCreator(fileCtn):
+        while True:
+            try:
+                file = "editedFile"
+                if fileCtn > 1:
+                    file = file+str(fileCtn)
+                f = open(file,"x")
+                f.close()
+                f = open(file, "a")
+                break
+            except FileExistsError:
+                fileCtn += 1
+        return f
+
+    def newFile(line, io):
+        if not line:
+            b = 2
+        else:
+            io.write(line)
+        return
